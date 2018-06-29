0bc5471f70a0e36eeb7586cd2d69a1911e582aa9
rascada <carftjr@gmail.com>
2018-06-29 17:19:40 +0200

    little bigger default parallax speed

--- projects/alternation/src/lib/alternation.service.ts
+++ projects/alternation/src/lib/alternation.service.ts
@@ -8,18 +8,23 @@ import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
 export class Alternation {
   constructor(private dispatcher: ScrollDispatcher, private zone: NgZone) { }
   time: BehaviorSubject<string> = new BehaviorSubject('.2s');
-  speed: BehaviorSubject<number> = new BehaviorSubject(7);
+  speed: BehaviorSubject<number> = new BehaviorSubject(3);
   alternation: Subject<number> = new Subject();
 
   position(value) {
     return { value, params: { next: value || 0, time: this.time.value } };
   }
 
-  init(delay = 100) {
-    this.dispatcher.scrolled(delay).subscribe((scrollable: CdkScrollable) => {
+  init(options: { delay?: number, speed?: number } = { delay: 100, speed: null }) {
+
+    if (options.speed) {
+      this.speed.next(options.speed);
+    }
+
+    this.dispatcher.scrolled(options.delay).subscribe((scrollable: CdkScrollable) => {
       if (scrollable) {
         const $scrollable: HTMLElement = scrollable.getElementRef().nativeElement;
-        let next =  -Math.round(($scrollable.scrollTop / this.speed.value));
+        let next =  -Math.round((($scrollable.scrollTop / 10) * this.speed.value));
 
         if (next) {
           this.zone.run(() => this.alternation.next(next));
